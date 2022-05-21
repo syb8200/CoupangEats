ed9f04d31205f4ce3870ede24465b6238f6c2f87
syb8200 <syb8200@naver.com>
2022-05-21 16:41:08 +0900

    템플릿 적용

---
+++ app/src/main/java/com/risingcamp/coupangeats/src/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.risingcamp.coupangeats.src
+
+import android.os.Bundle
+import com.risingcamp.coupangeats.config.BaseActivity
+import com.risingcamp.coupangeats.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
